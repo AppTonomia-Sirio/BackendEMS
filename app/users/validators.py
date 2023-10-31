95f952b5a010cb9c4eb1e002158c3aa23557c848
Notmemayeb <notmemaybeatwork@gmail.com>
2023-10-31 09:30:08 +0100

    Password validation

--- app/users/validators.py
+++ app/users/validators.py
@@ -33,3 +33,17 @@ class DocumentValidator(BaseValidator):
         else:
             raise ValidationError(self.message, code=self.code)
 
+
+class PasswordValidator(BaseValidator):
+    message = 'Password is not valid'
+    code = 'invalid'
+
+    def __init__(self, password=None):
+        super().__init__(password)
+
+    def __call__(self, password):
+        if password is not None:
+            if len(password) < 4:
+                raise ValidationError(self.message, code=self.code)
+        else:
+            raise ValidationError(self.message, code=self.code)
