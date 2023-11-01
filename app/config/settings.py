da867bbe5c96ed9c70bc3729bddaff4df4284a2a
blazaid <alberto.da@gmail.com>
2023-11-01 20:58:03 +0100

    Preparando la aplicación para el despliegue con GitHub Actions

--- app/config/settings.py
+++ app/config/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+if 'WEBSITE_HOSTNAME' in os.environ: # Running on Azure
+    from .azure import *
