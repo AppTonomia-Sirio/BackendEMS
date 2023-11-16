112bfc5bcbb11beb5ed4741b21f4b1099df7969b
Rowiz49 <adrianpuyetm@gmail.com>
2023-11-16 15:57:01 +0100

    Add documentation docs: add documentation for filters

--- app/users/filters.py
+++ app/users/filters.py
@@ -6,9 +6,13 @@ class UserFilter(django_filters.FilterSet):
     home = django_filters.CharFilter(field_name="home__name")
     order = django_filters.OrderingFilter(
         fields=(
-            ('created_by', 'created_by'),
+            ('created_at', 'created_at'),
+            ('name', 'name'),
+            ('status', 'status')
+            ('date_of_birth','date_of_birth')
         )
     )
     class Meta:
         model = CustomUser
         fields = ["status", "name", "surname", "email", "document", "date_of_birth", "created_at"]
+        #These are exact lookups
