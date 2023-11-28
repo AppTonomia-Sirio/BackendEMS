9eed7df5ee637614c5fb18fb548424628d039bb5
notmemayeb <notmemaybeatwork@gmail.com>
2023-11-28 15:35:21 +0100

    Reorged permissions for UserDetailView

--- app/users/permissions.py
+++ app/users/permissions.py
@@ -1,19 +1,24 @@
 from rest_framework import permissions
 
 
-class IsSuperUserToDelete(permissions.BasePermission):
+class UserDetailPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         # Read only(GET, HEAD, OPTIONS) for all users
         if request.method in permissions.SAFE_METHODS:
             return True
-        # Write only(POST, PATCH) for all users
-        if request.method in ('POST', 'PATCH'):
-            return True
+        # Write only(POST, PATCH, PUT) for admin. superuser and me
+        if request.method in ('POST', 'PATCH', 'PUT'):
+            return request.user and request.user.roles.filter(name='Admin').exists() or\
+                request.user.is_superuser or\
+                request.user.id == view.kwargs['id']
         # Delete only for superusers
         return request.user and request.user.is_superuser
 
 
 class IsAdminOrSuperUser(permissions.BasePermission):
     def has_permission(self, request, view):
-        # Check if user has role Admin
-        return request.user and request.user.roles.filter(name='Admin').exists() or request.user.is_superuser
+        return request.user and request.user.roles.filter(name='Admin').exists() or\
+            request.user.is_superuser
+
+
+
