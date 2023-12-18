fda4fd93302b587ee0a4147b4f6127e8565668af
notmemayeb <notmemaybeatwork@gmail.com>
2023-12-18 19:58:04 +0100

    False

--- app/config/azure.py
+++ app/config/azure.py
@@ -2,6 +2,7 @@ from .settings import *
 import os
 
 SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = False
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
