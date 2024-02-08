8ec250e009ca7b97c4f0b4c1cba018d852751dc0
notmemayeb <notmemaybeatwork@gmail.com>
2024-02-08 10:00:33 +0100

    Remade NNA and Staff user models, registered to admin panel

--- app/users/models.py
+++ app/users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from .validators import DocumentValidator, PasswordValidator
 from django.core.validators import MaxValueValidator, MinValueValidator
+from rest_framework.authtoken.models import Token
 from django.db import models
 
 
@@ -17,16 +18,29 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     created_at = models.DateTimeField(auto_now_add=True)
     id = models.AutoField(primary_key=True)
-
-    objects = CustomUserManager()
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name', 'surname','email','password']
+    REQUIRED_FIELDS = ['name', 'surname', 'password']
+
+    def save(self, *args, **kwargs):
+        is_new = self.pk is None
+        super().save(*args, **kwargs)
+        if is_new:
+            Token.objects.create(user=self)
 
     def __str__(self):
         return self.email
 
+
 class NNAUser(CustomUser):
+
+    class Meta:
+        verbose_name = 'NNA'
+        verbose_name_plural = 'NNAs'
+
     STATUS_CHOICES = (
         ('Active', 'Active'),
         ('Pending', 'Pending'),
@@ -45,22 +59,26 @@ class NNAUser(CustomUser):
     home = models.ForeignKey('Home', on_delete=models.PROTECT)
     status = models.CharField(max_length=255, choices=STATUS_CHOICES, default='Pending')
     gender = models.CharField(max_length=255, choices=GENDER_CHOICES, default='Undefined')
-    mentor = models.ManyToManyField('StaffUser', blank = True)
-    autonomy_level = models.IntegerField(default = 1, validators = [MaxValueValidator(10), MinValueValidator(1)])
-    is_tutor = models.BooleanField(default = False)
-    therapist = models.ForeignKey(StaffUser, on_delete=models.PROTECT)
+    mentors = models.ManyToManyField('StaffUser', blank=True, related_name='mentors')
+    therapist = models.ForeignKey('StaffUser', on_delete=models.PROTECT, blank=True, null=True)
+    autonomy_level = models.IntegerField(default=1, validators=[MaxValueValidator(10), MinValueValidator(1)])
+    is_tutor = models.BooleanField(default=False)
     entered_at = models.DateField()
-    
-    REQUIRED_FIELDS = ['name', 'surname','email','password','date_of_birth','home','status','gender']
+
+    REQUIRED_FIELDS = ['name', 'surname', 'password', 'date_of_birth', 'home', 'status', 'gender']
 
 
 class StaffUser(CustomUser):
 
+    class Meta:
+        verbose_name = 'Staff'
+        verbose_name_plural = 'Staff'
+
     homes = models.ManyToManyField("Home")
     roles = models.ManyToManyField("Role")
-    is_admin = models.BooleanField(default = False)
-    
-    REQUIRED_FIELDS = ['name', 'surname','email','password','homes',"roles", "is_admin"]
+    is_admin = models.BooleanField(default=False)
+
+    REQUIRED_FIELDS = ['name', 'surname', 'password', 'homes', "roles", "is_admin"]
 
 
 class Home(models.Model):
