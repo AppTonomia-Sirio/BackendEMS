b69ff9d0fcbe19c9aa7c994b431326857d1931e0
Rowiz49 <adrianpuyetm@gmail.com>
2024-02-09 17:23:05 +0100

    Refactor Serializers

--- app/config/settings.py
+++ app/config/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "django_filters",
     'polymorphic',
+    'rest_polymorphic'
     # Local
     "users",
 ]
--- app/users/serializers.py
+++ app/users/serializers.py
@@ -1,7 +1,92 @@
-"""from .models import CustomUser, Home, Role
+from .models import CustomUser, Home, Role, NNAUser, StaffUser
 from rest_framework import serializers
+from rest_polymorphic import PolymorphicSerializer
 
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'email', 'name', 'surname', 'password', 'is_superuser', 'created_at', 'is_staff')
+        extra_kwargs = {'password': {'write_only': True}, 'created_at': {'read_only': True}}
+    def create(self, validated_data):
+        required_fields = CustomUser.REQUIRED_FIELDS + CustomUser.USERNAME_FIELD
+        errors = {}
 
+        for field in required_fields:
+            if not validated_data.get(field):
+                errors[field] = 'This field is required'
+        if errors:
+            raise serializers.ValidationError(errors)
+
+        user = CustomUser(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            surname=validated_data['surname'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+class NNAUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NNAUser
+        fields = ('id', 'email', 'name', 'surname', 'password', 'created_at', 'document',
+                  'date_of_birth', 'home', 'status', 'gender', 'mentors', 'therapist',
+                  'autonomy_level', 'is_tutor', 'entered_at')
+        extra_kwargs = {'password': {'write_only': True}, 'created_at': {'read_only': True}}
+        def create(self, validated_data):
+            required_fields = NNAUser.REQUIRED_FIELDS + NNAUser.USERNAME_FIELD
+            errors = {}
+
+            for field in required_fields:
+                if not validated_data.get(field):
+                    errors[field] = 'This field is required'
+            if errors:
+                raise serializers.ValidationError(errors)
+            
+            user = NNAUser(
+                email=validated_data['email'],
+                name=validated_data['name'],
+                surname=validated_data['surname'],
+                document=validated_data['document'],
+                date_of_birth=validated_data['date_of_birth'],
+                home=validated_data['home'],
+            )
+            user.set_password(validated_data['password'])
+            user.save()
+            return user
+class StaffUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StaffUser
+        fields = ('id', 'email', 'name', 'surname', 'password', 'created_at',
+                  'homes', 'roles', 'is_admin')
+        extra_kwargs = {'password': {'write_only': True}, 'created_at': {'read_only': True}}
+    def create(self, validated_data):
+        required_fields = StaffUser.REQUIRED_FIELDS + StaffUser.USERNAME_FIELD
+        errors = {}
+
+        for field in required_fields:
+            if not validated_data.get(field):
+                errors[field] = 'This field is required'
+        if errors:
+            raise serializers.ValidationError(errors)
+        
+        user = NNAUser(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            surname=validated_data['surname'],
+        )
+        user.set_password(validated_data['password'])
+        user.roles.set(validated_data['roles'])
+        user.homes.set(validated_data['homes'])
+        user.save()
+        return user
+class UserPolymorphicSerializer(PolymorphicSerializer):
+    model_serializer_mapping = {
+        CustomUser: CustomUserSerializer,
+        NNAUser: NNAUserSerializer,
+        StaffUser: StaffUserSerializer
+    }
+"""
 class UserSerializer(serializers.ModelSerializer):
     # Serializer for User model
     class Meta:
@@ -33,7 +118,7 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
-
+"""
 class HomeSerializer(serializers.ModelSerializer):
     # Serializer for Location model
     class Meta:
@@ -46,4 +131,3 @@ class RoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
         fields = ('id', 'name')
-"""
